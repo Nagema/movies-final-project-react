23d31e2111d0275386d4acc043639e9d2efebcde
angelica <angelica.cavero@gmail.com>
2022-11-13 14:06:47 +0100

    logout button styles added

--- src/components/logout/Logout.jsx
+++ src/components/logout/Logout.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom'
-import { logoutUser } from '../../redux/auth/auth.actions';
-
+import React from "react";
+import "./Logout.css";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logoutUser } from "../../redux/auth/auth.actions";
 
 const Logout = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const logOutButton = () => {
     dispatch(logoutUser(navigate));
-  }
+  };
 
   return (
-    <button onClick={logOutButton}>Logout</button>
-  )
-}
+    <button className="logout-button" onClick={logOutButton}>
+      Logout
+    </button>
+  );
+};
 
-export default Logout
+export default Logout;
