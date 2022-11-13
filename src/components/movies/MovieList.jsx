84a5f7c1d70d7744488b40cb5e91ac4580cbe9d0
angelica <angelica.cavero@gmail.com>
2022-11-13 23:30:03 +0100

    fix

--- src/components/movies/MovieList.jsx
+++ src/components/movies/MovieList.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-// import axios from "axios";
+import React, { useEffect } from "react";
 import "./MovieList.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getMovies } from "../../redux/movies/movies.functions";
