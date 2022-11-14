b5fdc18da5fa3a5ac4ad409fa6d92821dbeab9ce
angelica <angelica.cavero@gmail.com>
2022-11-14 18:59:42 +0100

    search logic added

--- src/components/movies/MovieList.jsx
+++ src/components/movies/MovieList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./MovieList.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getMovies } from "../../redux/movies/movies.functions";
@@ -7,16 +7,32 @@ import MovieCard from "../movie-card/MovieCard";
 const MovieList = () => {
   const dispatch = useDispatch();
   const { movies, isLoading, error } = useSelector((state) => state.movies);
+  const [searchInputValue, setSearchInputValue] = useState("");
 
   useEffect(() => {
     dispatch(getMovies());
   }, []);
+  const filteredMovies = [];
+
+  movies.map((movie) => {
+    if (movie.title.toLowerCase().includes(searchInputValue.toLowerCase())) {
+      filteredMovies.push(movie);
+    }
+  });
+
+  const moviesInfo = filteredMovies.length ? filteredMovies : movies;
 
   return (
     <>
       {!isLoading && (
         <div className="search-wrapper">
-          <input type="search" className="input-search" placeholder="Search" />
+          <input
+            type="search"
+            className="input-search"
+            placeholder="Search"
+            value={searchInputValue}
+            onChange={(e) => setSearchInputValue(e.target.value)}
+          />
         </div>
       )}
       <div className="movies-container">
@@ -25,7 +41,7 @@ const MovieList = () => {
             <span className="loader"></span>
           </div>
         ) : (
-          movies.map((movie) => {
+          moviesInfo.map((movie) => {
             return <MovieCard key={movie._id} movie={movie} />;
           })
         )}
